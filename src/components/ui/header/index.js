31a77ce8c3d765a44768cbfd3421f46109b4592f
Peter Ihimire <peterihimire@gmail.com>
2022-02-12 09:06:25 +0100

    Leaned components

--- src/components/ui/header/index.js
+++ src/components/ui/header/index.js
@@ -1,57 +1,11 @@
 import React from "react";
 
-import { NavLink } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import * as actions from "../../../store/actions";
-import { ReactComponent as Logo } from "../../../assets/images/SFS-LOGOS-4-150x150.svg";
 import "./styles.scss";
 
 const Header = () => {
-  const dispatch = useDispatch();
-
-  const { currentUser } = useSelector((state) => {
-    return {
-      currentUser: state.auth.currentUser,
-    };
-  });
-
-  const logout = () => {
-    dispatch(actions.setCurrentUser(null));
-  };
-
   return (
-    <header className="header">
-      <nav>
-        <div className="brand-logo">
-          <Logo />
-        </div>
-
-        <div className="nav-links">
-          <ul>
-            {!currentUser && (
-              <li>
-                <NavLink to="/login">Login</NavLink>
-              </li>
-            )}
-
-            {!currentUser && (
-              <li>
-                <NavLink to="/signup" className="signup-btn">
-                  Signup
-                </NavLink>
-              </li>
-            )}
-
-            {currentUser && (
-              <li>
-                <span className="log-out" onClick={logout}>
-                  Logout
-                </span>
-              </li>
-            )}
-          </ul>
-        </div>
-      </nav>
+    <header className='header'>
+      <h3>Header</h3>
     </header>
   );
 };
--- src/layouts/auth/index.js
+++ src/layouts/auth/index.js
@@ -1,33 +1,20 @@
-import React, { useEffect } from "react";
+import React from "react";
 
-import { ReactComponent as Logo } from "../../assets/images/SFS-LOGOS-4-150x150.svg";
-import { NavLink, useLocation } from "react-router-dom";
+
 import "./styles.scss";
 
 const AuthLayout = ({ children }) => {
-  const location = useLocation();
 
-  useEffect(() => {}, [location.pathname]);
 
   return (
     <div className="auth-layout">
       <header className="header">
-        <div className="logo">
-          <NavLink to="/">
-            <Logo />
-          </NavLink>
-        </div>
+       
 
         <div className="header__lead">
-          {location.pathname === "/login" && <span>Welcome to:</span>}
+       
 
-          {location.pathname === "/signup" && <span>Get started with</span>}
-
-          {location.pathname === "/account-success" && (
-            <span>You are almost there with</span>
-          )}
-
-          <h1>SFS Capital</h1>
+          <h1>Image Half</h1>
         </div>
       </header>
 
